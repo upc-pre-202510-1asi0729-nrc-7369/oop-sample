522e38e018e51547d65a7ab171630ac2c4da3b72
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2025-09-14 21:54:08 -0500

    chore: setups project

---
+++ catch-ups/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {Layout} from './shared/presentation/components/layout/layout';
+
+@Component({
+  selector: 'app-root',
+  imports: [Layout],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('catch-up');
+}
