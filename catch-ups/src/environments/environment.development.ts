1341af93a79a6f6dd8468c0493a030e461031bb5
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2025-09-14 22:04:42 -0500

    revert: deshacer cambios

--- catch-ups/src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
-  newsProviderNewsEndpointPath: '/top-headlines',
-  newsProviderSourcesEndpointPath: '/sources',
-  newsProviderApiKey: 'fecf4feeffa64e4da682e7d268612ce5',
-  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
-};
